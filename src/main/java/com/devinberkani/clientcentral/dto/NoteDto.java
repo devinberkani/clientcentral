2efd6c72d5295818319b7b303816dae4d911cd76
Devin Berkani <devinberkani@gmail.com>
2023-03-26 10:20:48 -0400

    update note character limit to 255 characters

--- src/main/java/com/devinberkani/clientcentral/dto/NoteDto.java
+++ src/main/java/com/devinberkani/clientcentral/dto/NoteDto.java
@@ -3,6 +3,7 @@ package com.devinberkani.clientcentral.dto;
 import com.devinberkani.clientcentral.entity.Client;
 import com.devinberkani.clientcentral.entity.FileAttachment;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -20,6 +21,7 @@ public class NoteDto {
     private LocalDateTime createdOn;
     private LocalDateTime updatedOn;
     @NotEmpty(message = "Note content should not be empty")
+    @Size(max = 255, message = "Content should be no more than 255 characters")
     private String content;
     private Client client;
     private List<FileAttachment> files = new ArrayList<>();
