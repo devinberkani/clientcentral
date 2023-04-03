8cc896416f240f3a9f8ff4a261c3a7ea1d8899dc
Devin Berkani <devinberkani@gmail.com>
2023-04-03 14:55:09 -0400

    begin implementation of spring security

---
+++ src/main/java/com/devinberkani/clientcentral/config/WebSpringSecurity.java
@@ -0,0 +1,9 @@
+package com.devinberkani.clientcentral.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity // used to enable spring security's web security support and provide the spring mvc integration
+public class WebSpringSecurity {
+}
