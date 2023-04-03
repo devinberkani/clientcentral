c3ac31be19093732ff5375ad8aa5328eab67686f
Devin Berkani <devinberkani@gmail.com>
2023-04-03 18:08:44 -0400

    implement login functionality with spring security

--- src/main/java/com/devinberkani/clientcentral/config/WebSpringSecurity.java
+++ src/main/java/com/devinberkani/clientcentral/config/WebSpringSecurity.java
@@ -1,9 +1,60 @@
 package com.devinberkani.clientcentral.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity // used to enable spring security's web security support and provide the spring mvc integration
 public class WebSpringSecurity {
+
+    private final UserDetailsService userDetailsService;
+
+    public WebSpringSecurity(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+//    @Bean
+//    public static PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeHttpRequests((authorize) ->
+                        authorize
+                                .requestMatchers(new AntPathRequestMatcher("/js/**")).permitAll()
+                                .requestMatchers(new AntPathRequestMatcher("/css/**")).permitAll()
+                                .requestMatchers(new AntPathRequestMatcher("/images/**")).permitAll()
+                                .requestMatchers(new AntPathRequestMatcher("/register/**")).permitAll()
+                                .requestMatchers(new AntPathRequestMatcher("/")).permitAll()
+                                .requestMatchers(new AntPathRequestMatcher("/admin/**")).authenticated()
+                                .anyRequest().authenticated() // any other request must be authenticated
+                )
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/admin/dashboard")
+                        .loginProcessingUrl("/login")
+                        .permitAll()
+                ).logout(logout -> logout.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                        .permitAll()
+                );
+        return http.build();
+    }
+
+//    @Autowired
+//    public void configureGlobal(AuthenticationManagerBuilder builder) throws Exception {
+//        builder.userDetailsService(userDetailsService)
+//                .passwordEncoder(passwordEncoder());
+//    }
+
 }
