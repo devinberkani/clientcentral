4d0f0bf05b0c10f1ca02c7ec8a75bae769717a11
Devin Berkani <devinberkani@gmail.com>
2023-04-05 15:17:48 -0400

    configure demo user login

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -4,3 +4,9 @@ setTimeout(function() {
         alerts[i].style.display = 'none';
     }
 }, 7000);
+
+function demoLogin() {
+    document.getElementById("demoUsername").value = "demo@gmail.com";
+    document.getElementById("demoPassword").value = "demoPassword";
+    document.getElementById("demoLoginForm").submit();
+}
